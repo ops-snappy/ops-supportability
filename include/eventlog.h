OpenSwitchNOS/openswitch-ops-supportability

new: dev: Event Log Framework Implementation
Change-Id: I68313df6d8d6452b39f35d7a1e867a6211aa88b0
Signed-off-by: Renjith <renjith.v@hpe.com>

include/eventlog.h
@@ -35,15 +35,29 @@
 #ifndef __EVENTLOG_H_
 #define __EVENTLOG_H_
 
+#define TRUE 1
+#define FALSE 0
+#define MESSAGE_OPS_EVT "50c0fa81c2a545ec982a54293f1b1945"
+#define MAX_CATEGORIES_PER_DAEMON 255
+#define KEY_VALUE_SIZE 128
+#define MAX_LOG_STR 480
+#define MAX_EVENT_NAME_SIZE 64
+#define MAX_SEV_NAME_SIZE 10
+#define MAX_EVENT_TABLE_SIZE 500
+#define EVENT_NAME_DELIMITER_STR "EV_TBD_TBD"
+#define EVENT_YAML_FILE "/etc/openswitch/supportability/events.yaml"
+#define KV(...) key_value_string(__VA_ARGS__)
 
 
-/************************************************************************//**
- * Logs the Event in the Logging Infra
- *
- * @param[in] log_message   : Event Message to be Logged
- *
- * @return int status of log action
- ***************************************************************************/
-extern int event_log (const char *log_message);
+typedef struct {
+    int event_id;
+    char event_name[MAX_EVENT_NAME_SIZE];
+    char severity[MAX_SEV_NAME_SIZE];
+    int num_of_keys;
+    char event_description[MAX_LOG_STR];
+    } event;
 
+extern int event_log_init(char *event_name);
+extern int log_event(char *ev_name,...);
+extern char *key_value_string(char *s1, ...);
 #endif /* __EVENTLOG_H_ */

ops-snappy/ops-supportability

include/eventlog.h
@@ -1,5 +1,5 @@
 /*
- Copyright (C) 2015 Hewlett-Packard Development Company, L.P.
+ Copyright (C) 2016 Hewlett-Packard Development Company, L.P.
  All Rights Reserved.
 
     Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -35,15 +35,29 @@
 #ifndef __EVENTLOG_H_
 #define __EVENTLOG_H_
 
+#define TRUE 1
+#define FALSE 0
+#define MESSAGE_OPS_EVT "50c0fa81c2a545ec982a54293f1b1945"
+#define MAX_CATEGORIES_PER_DAEMON 99
+#define KEY_VALUE_SIZE 128
+#define MAX_LOG_STR 480
+#define MAX_EVENT_NAME_SIZE 64
+#define MAX_SEV_NAME_SIZE 10
+#define MAX_EVENT_TABLE_SIZE 500
+#define EVENT_NAME_DELIMITER_STR "EV_TBD_TBD"
+#define EVENT_YAML_FILE "/etc/openswitch/supportability/ops_events.yaml"
+#define EV_KV(...) key_value_string(__VA_ARGS__)
 
 
-/************************************************************************//**
- * Logs the Event in the Logging Infra
- *
- * @param[in] log_message   : Event Message to be Logged
- *
- * @return int status of log action
- ***************************************************************************/
-extern int event_log (const char *log_message);
+typedef struct {
+    int event_id;
+    char event_name[MAX_EVENT_NAME_SIZE];
+    char severity[MAX_SEV_NAME_SIZE];
+    int num_of_keys;
+    char event_description[MAX_LOG_STR];
+    } event;
 
+extern int event_log_init(char *category);
+extern int log_event(char *ev_name,...);
+extern char *key_value_string(char *s1, ...);
 #endif /* __EVENTLOG_H_ */