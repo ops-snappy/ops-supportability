OpenSwitchNOS/openswitch-ops-supportability

new: dev:#3712 Show Vlog CLI Implementation
Change-Id: I01ea14545dc21c7f8b2ea5fa7831ff74332b879c
Signed-off-by: bharathdoopati <sribharath.doopati@hpe.com>

include/supportability_vty.h
@@ -32,6 +32,8 @@ extern struct cmd_element cli_platform_show_tech_list_cmd;
 extern struct cmd_element cli_platform_show_tech_feature_cmd;
 extern struct cmd_element cli_platform_show_events_cmd;
 extern struct cmd_element cli_platform_show_core_dump_cmd;
-
-
+extern struct cmd_element cli_platform_show_vlog_cmd;
+extern struct cmd_element cli_platform_show_vlog_list_cmd;
+extern struct cmd_element cli_platform_showvlog_feature_cmd;
+extern struct cmd_element cli_config_vlog_set_cmd;
 #endif /* _SUPPORTABILITY_VTY_H_ */

src/cli/supportability_vty.c
@@ -58,4 +58,8 @@ cli_post_init()
   install_element (ENABLE_NODE, &cli_platform_show_events_cmd);
   install_element (ENABLE_NODE, &vtysh_diag_dump_cmd);
   install_element (ENABLE_NODE, &vtysh_diag_dump_list_cmd);
+  install_element (ENABLE_NODE, &cli_platform_show_vlog_cmd);
+  install_element (ENABLE_NODE, &cli_platform_show_vlog_list_cmd);
+  install_element (ENABLE_NODE, &cli_platform_showvlog_feature_cmd);
+  install_element (CONFIG_NODE, &cli_config_vlog_set_cmd);
 }

ops-snappy/ops-supportability

include/supportability_vty.h
@@ -32,6 +32,8 @@ extern struct cmd_element cli_platform_show_tech_list_cmd;
 extern struct cmd_element cli_platform_show_tech_feature_cmd;
 extern struct cmd_element cli_platform_show_events_cmd;
 extern struct cmd_element cli_platform_show_core_dump_cmd;
-
-
+extern struct cmd_element cli_platform_show_vlog_config_cmd;
+extern struct cmd_element cli_platform_show_vlog_config_list_cmd;
+extern struct cmd_element cli_platform_show_vlog_feature_cmd;
+extern struct cmd_element cli_config_vlog_set_cmd;
 #endif /* _SUPPORTABILITY_VTY_H_ */

src/cli/supportability_vty.c
@@ -58,4 +58,8 @@ cli_post_init()
   install_element (ENABLE_NODE, &cli_platform_show_events_cmd);
   install_element (ENABLE_NODE, &vtysh_diag_dump_cmd);
   install_element (ENABLE_NODE, &vtysh_diag_dump_list_cmd);
+  install_element (ENABLE_NODE, &cli_platform_show_vlog_config_cmd);
+  install_element (ENABLE_NODE, &cli_platform_show_vlog_config_list_cmd);
+  install_element (ENABLE_NODE, &cli_platform_show_vlog_feature_cmd);
+  install_element (CONFIG_NODE, &cli_config_vlog_set_cmd);
 }