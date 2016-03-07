1fa362a97abe29145756343c1096632f3f913be1
Renjith <renjith.v@hpe.com>
2016-03-07 05:59:12 -0800

    new: dev: CT test for show events filters
    Add CT cases for show events CLI filters
    
    Change-Id: I8de521ca3413fd9a7d08d56a90731888b4f1f749
    Signed-off-by: Renjith <renjith.v@hpe.com>

--- test/eventlog_test.py
+++ test/eventlog_test.py
@@ -23,6 +23,226 @@ topoDict = {"topoExecution": 1000,
             "topoDevices": "dut01",
             "topoFilters": "dut01:system-category:switch"}
 
+
+# Negative test case for show events severity filter
+def evtlogfilterSeverity_cli(dut01):
+    LogOutput('info', "\n############################################")
+    LogOutput('info', " Running Event Log Test Script")
+    LogOutput('info', "############################################\n")
+    overallBuffer = []
+    finalReturnCode = 0
+
+    #Get in to vtysh shell
+    retStruct = dut01.VtyshShell(enter=True)
+    overallBuffer.append(retStruct.buffer())
+    returnCode = retStruct.returnCode()
+    if returnCode != 0:
+        LogOutput('error',"Failed to enter vtysh prompt")
+        for curLine in overallBuffer:
+                LogOutput('info',str(curLine))
+        return False
+    # enable lldp
+    dut01.DeviceInteract(command="configure terminal")
+    dut01.DeviceInteract(command="lldp enable")
+    #disable lldp
+    dut01.DeviceInteract(command="no lldp enable")
+    dut01.DeviceInteract(command="lldp timer 200")
+
+    dut01.DeviceInteract(command="end")
+
+    print "-"*10
+    print "=====----"
+    print "-"*10
+
+    returnDevInt = dut01.DeviceInteract(command="show events severity emer")
+    retStruct = dut01.VtyshShell(enter=False)
+    overallBuffer.append(retStruct.buffer())
+    returnCode = retStruct.returnCode()
+    if returnCode !=0:
+        LogOuput('error',"Failed to exit vtysh prompt")
+        for curLine in overallBuffer:
+            LogOutput('info',str(curLine))
+        return False
+    finalReturnCode = returnDevInt['returnCode']
+    overallBuffer.append(returnDevInt['buffer'])
+
+    if finalReturnCode != 0:
+        LogOutput('error',
+                "Failed to run show events " +
+                "on device" + str(dut01))
+        return False
+    else:
+        if "No event match the filter provided" in returnDevInt.get('buffer'):
+            LogOutput('info',
+                    "Test Case passed, please refer output below")
+            for outputs in overallBuffer:
+                LogOutput('info', str(outputs))
+            return True
+        else:
+            return False
+
+# Negative test case for show events category filter
+def evtlogfilterCategory_cli(dut01):
+    LogOutput('info', "\n############################################")
+    LogOutput('info', " Running Event Log Test Script")
+    LogOutput('info', "############################################\n")
+    overallBuffer = []
+    finalReturnCode = 0
+
+    #Get in to vtysh shell
+    retStruct = dut01.VtyshShell(enter=True)
+    overallBuffer.append(retStruct.buffer())
+    returnCode = retStruct.returnCode()
+    if returnCode != 0:
+        LogOutput('error',"Failed to enter vtysh prompt")
+        for curLine in overallBuffer:
+                LogOutput('info',str(curLine))
+        return False
+    # enable lldp
+    dut01.DeviceInteract(command="configure terminal")
+    dut01.DeviceInteract(command="lldp enable")
+    #disable lldp
+    dut01.DeviceInteract(command="no lldp enable")
+    dut01.DeviceInteract(command="lldp timer 200")
+
+    dut01.DeviceInteract(command="end")
+
+    print "-"*10
+    print "=====----"
+    print "-"*10
+
+    returnDevInt = dut01.DeviceInteract(command="show events category abc")
+    retStruct = dut01.VtyshShell(enter=False)
+    overallBuffer.append(retStruct.buffer())
+    returnCode = retStruct.returnCode()
+    if returnCode !=0:
+        LogOuput('error',"Failed to exit vtysh prompt")
+        for curLine in overallBuffer:
+            LogOutput('info',str(curLine))
+        return False
+    finalReturnCode = returnDevInt['returnCode']
+    overallBuffer.append(returnDevInt['buffer'])
+
+    if finalReturnCode != 0:
+        LogOutput('error',
+                "Failed to run show events " +
+                "on device" + str(dut01))
+        return False
+    else:
+        if "No event match the filter provided" in returnDevInt.get('buffer'):
+            LogOutput('info',
+                    "Test Case passed, please refer output below")
+            for outputs in overallBuffer:
+                LogOutput('info', str(outputs))
+            return True
+        else:
+            return False
+
+# Negative test case for show events event id filter
+def evtlogfilterEventID_cli_fail(dut01):
+    LogOutput('info', "\n############################################")
+    LogOutput('info', " Running Event Log Test Script 1.2")
+    LogOutput('info', "############################################\n")
+    overallBuffer = []
+    finalReturnCode = 0
+
+    #Get in to vtysh shell
+    retStruct = dut01.VtyshShell(enter=True)
+    overallBuffer.append(retStruct.buffer())
+    returnCode = retStruct.returnCode()
+    if returnCode != 0:
+        LogOutput('error',"Failed to enter vtysh prompt")
+        for curLine in overallBuffer:
+                LogOutput('info',str(curLine))
+        return False
+    print "-"*10
+    print "=====----"
+    print "-"*10
+    returnDevInt = dut01.DeviceInteract(command="show events event-id 999998")
+    retStruct = dut01.VtyshShell(enter=False)
+    overallBuffer.append(retStruct.buffer())
+    returnCode = retStruct.returnCode()
+    if returnCode !=0:
+        LogOuput('error',"Failed to exit vtysh prompt")
+        for curLine in overallBuffer:
+            LogOutput('info',str(curLine))
+        return False
+    finalReturnCode = returnDevInt['returnCode']
+    overallBuffer.append(returnDevInt['buffer'])
+
+    if finalReturnCode != 0:
+        LogOutput('error',
+                "Failed to run show events " +
+                "on device" + str(dut01))
+        return False
+    else:
+        if "No event match the filter provided" in returnDevInt.get('buffer'):
+            LogOutput('info',
+                    "Test Case passed, please refer output below")
+            for outputs in overallBuffer:
+                LogOutput('info', str(outputs))
+            return True
+        else:
+            return False
+
+# Positive test case for show events event id filter
+def evtlogfilterEventID_cli_pass(dut01):
+    LogOutput('info', "\n############################################")
+    LogOutput('info', " Running Event Log Test Script 1.1")
+    LogOutput('info', "############################################\n")
+    overallBuffer = []
+    finalReturnCode = 0
+
+    #Get in to vtysh shell
+    retStruct = dut01.VtyshShell(enter=True)
+    overallBuffer.append(retStruct.buffer())
+    returnCode = retStruct.returnCode()
+    if returnCode != 0:
+        LogOutput('error',"Failed to enter vtysh prompt")
+        for curLine in overallBuffer:
+                LogOutput('info',str(curLine))
+        return False
+
+    # enable lldp
+    dut01.DeviceInteract(command="configure terminal")
+    dut01.DeviceInteract(command="lldp enable")
+    #disable lldp
+    dut01.DeviceInteract(command="no lldp enable")
+    dut01.DeviceInteract(command="lldp timer 100")
+
+    dut01.DeviceInteract(command="end")
+
+    print "-"*10
+    print "=====----"
+    print "-"*10
+
+    returnDevInt = dut01.DeviceInteract(command="show events event-id 1003")
+    retStruct = dut01.VtyshShell(enter=False)
+    overallBuffer.append(retStruct.buffer())
+    returnCode = retStruct.returnCode()
+    if returnCode !=0:
+        LogOuput('error',"Failed to exit vtysh prompt")
+        for curLine in overallBuffer:
+            LogOutput('info',str(curLine))
+        return False
+    finalReturnCode = returnDevInt['returnCode']
+    overallBuffer.append(returnDevInt['buffer'])
+
+    if finalReturnCode != 0:
+        LogOutput('error',
+                "Failed to run show events " +
+                "on device" + str(dut01))
+        return False
+    else:
+        if "Configured LLDP tx-timer with" and "LLDP Disabled" not in returnDevInt.get('buffer'):
+            LogOutput('info',
+                    "Test Case passed, please refer output below")
+            for outputs in overallBuffer:
+                LogOutput('info', str(outputs))
+            return True
+        else:
+            return False
+
 def evtlogfeature_cli(dut01):
     LogOutput('info', "\n############################################")
     LogOutput('info', " Running Event Log Test Script")
@@ -94,11 +314,45 @@ class Test_ft_evtlog_feature:
         # Terminate all nodes
         Test_ft_evtlog_feature.topoObj.terminate_nodes()
 
-
-    def test_case(self):
+    # Test basic show events command
+    def test_show_events(self):
         dut01Obj = self.topoObj.deviceObjGet(device="dut01")
         retValue = evtlogfeature_cli(dut01Obj)
         if retValue == 1:
             LogOutput('info', "Event log  CLI -passed")
         else:
             LogOutput('info', "Event log CLI -failed")
+
+    # Test show events event ID FIlter (Positive & negative case)
+    def test_show_events_eventID(self):
+        dut01Obj = self.topoObj.deviceObjGet(device="dut01")
+        # Positive test case
+        retValue = evtlogfilterEventID_cli_pass(dut01Obj)
+        if retValue == 1:
+            LogOutput('info', "Event log event ID filter positive case -passed")
+        else:
+            LogOutput('info', "Event log event ID filter positive case -failed")
+        # Negative
+        retValue = evtlogfilterEventID_cli_fail(dut01Obj)
+        if retValue == 1:
+            LogOutput('info', "Event log  event ID filter negative case -passed")
+        else:
+            LogOutput('info', "Event log event ID filter negative case -failed")
+
+    # Test show events event category Filter(negative case)
+    def test_show_events_category(self):
+        dut01Obj = self.topoObj.deviceObjGet(device="dut01")
+        retValue = evtlogfilterCategory_cli(dut01Obj)
+        if retValue == 1:
+            LogOutput('info', "Event log category filter  CLI -passed")
+        else:
+            LogOutput('info', "Event log category filter CLI -failed")
+
+    # Test show events severity negative test case
+    def test_show_events_severity(self):
+        dut01Obj = self.topoObj.deviceObjGet(device="dut01")
+        retValue = evtlogfilterSeverity_cli(dut01Obj)
+        if retValue == 1:
+            LogOutput('info', "Event log category filter  CLI -passed")
+        else:
+            LogOutput('info', "Event log category filter CLI -failed")
